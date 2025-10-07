ea6cd887e03612782aa3bee84e94ba7bc392121c
rajaram.murali <rajaram.murali@iotfal.in>
2025-10-07 23:03:41 +0530

    updated values apps and for new version

--- apps/app1/server.js
+++ apps/app1/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
-app.get('/', (_req, res) => res.send('Hello from App1 (Node.js)!'));
+app.get('/', (_req, res) => res.send('Hello from App1 (Node.js)! New Version'));
 app.listen(port, () => console.log(`App1 listening on ${port}`));
--- apps/app2/app.py
+++ apps/app2/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.get("/")
 def hello():
-    return "Hello from App2 (Flask)!"
+    return "Hello from App2 (Flask)! New Version"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.getenv("PORT", "8080")))
